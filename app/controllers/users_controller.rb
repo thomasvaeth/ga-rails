93b97245bb37f91ce9a6374812817b0980d06909
Todd Best <toddbest2004@gmail.com>
2015-11-18 16:29:04 -0800

    adding session control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,20 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		@user = User.create user_params
+		if @user
+			session[:user_id] = @user.id
+			flash[:success] = "User created!!"
+			redirect_to root_path
+		else
+			flash[:danger] = "Credentials Invalid!!"
+			redirect_to signup_path
+		end
+	end
+
+	private
+
+	def user_params
+	  params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
 	end
 end
