7e8bcb3ba55ca0ca02dfcf20651a08d188f1cd1f
Andrew Mestas <andrew.mestas.dev@gmail.com>
2015-11-19 14:26:57 -0800

    added auto pop address

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
   get '/users/newAddress' => 'users#newAddress'
+  get '/location' => 'main#location'
   resource :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
