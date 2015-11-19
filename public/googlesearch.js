520a1c7aefb23126c40575fdd6cc157275105759
Todd Best <toddbest2004@gmail.com>
2015-11-19 11:37:50 -0800

    Starting the address save

--- public/googlesearch.js
+++ public/googlesearch.js
@@ -7,4 +7,14 @@ var from = document.getElementById('from');
 autocomplete = new google.maps.places.Autocomplete(from, options);
 var to = document.getElementById('to')
 autocomplete2 = new google.maps.places.Autocomplete(to, options);
+
+$("#saveFrom").click(function(){saveAddress("#from",$(this))})
+$("#saveTo").click(function(){saveAddress("#to", $(this))})
 })
+
+function saveAddress(elementName, button){
+	var address = $(elementName).val()
+	$.ajax({url:"/users/newAddress", data:{address:address}}).done(function(){
+		button.prop("disabled", true).val("Saved!")
+	})
+}
