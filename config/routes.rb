94e08aec5a933d402316844b6074614348902d7e
Andrew Mestas <andrew.mestas.dev@gmail.com>
2015-11-18 16:15:55 -0800

    seeded states and modified index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'main#index'
   get '/' => 'main#index'
   get '/results' => 'main#results'
+  post '/results' => 'main#results'
   resources :user
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
