69767830c4cd11abb5e153461f574471f16baf7e
Todd Best <toddbest2004@gmail.com>
2015-11-19 19:23:18 -0800

    Database now accurately adds count, uberfare, and lyftfares after each search.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118223932) do
+ActiveRecord::Schema.define(version: 20151119232539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,6 @@ ActiveRecord::Schema.define(version: 20151118223932) do
   create_table "addresses", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "address"
-    t.string   "city"
-    t.string   "state"
-    t.string   "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -30,11 +27,12 @@ ActiveRecord::Schema.define(version: 20151118223932) do
 
   create_table "states", force: :cascade do |t|
     t.string   "state"
-    t.string   "count"
     t.string   "milestotal"
-    t.string   "faretotal"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "uberfare"
+    t.float    "lyftfare"
+    t.integer  "count"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,314 +11,314 @@ states = State.create([
     "state":"Alabama",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
  
     "state":"Alaska",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
  
     "state":"Arizona",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
  
     "state":"Arkansas",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
  
     "state":"California",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
  
     "state":"Colorado",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
    },
   {
  
     "state":"Connecticut",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
  
     "state":"District of Columbia",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
  
     "state":"Delaware",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Florida",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Georgia",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Hawaii",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Idaho",
     "count":0,
     "milestotal":0,
-    "faretotal":0,
+    "uberfare":0, "lyftfare":0,
   },
   {
     "state":"Illinois",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Indiana",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Iowa",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Kansas",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Kentucky",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Louisiana",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Maine",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Maryland",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Massachusetts",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Michigan",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Minnesota",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Mississippi",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Missouri",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Montana",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Nebraska",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Nevada",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"New Hampshire",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"New Jersey",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"New Mexico",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"New York",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"North Carolina",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"North Dakota",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Ohio",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Oklahoma",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Oregon",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
      },
   {
     "state":"Pennsylvania",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Rhode Island",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"South Carolina",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"South Dakota",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Tennessee",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
     },
   {
     "state":"Texas",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Utah",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Vermont",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Virginia",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Washington",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"West Virginia",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Wisconsin",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
   },
   {
     "state":"Wyoming",
     "count":0,
     "milestotal":0,
-    "faretotal":0
+    "uberfare":0, "lyftfare":0
    }
 ])
