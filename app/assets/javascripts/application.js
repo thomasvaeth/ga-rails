7c8404799549f5e1b327660fb4b2fa3499e346dd
thomasvaeth <thomas.vaeth@gmail.com>
2015-11-21 16:34:53 -0800

    Ready to start CSS.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,64 @@
 //= require turbolinks
 //= require_tree .
 
+$(document).ready(function() {
+	// Full Page
+	$('#fullpage').fullpage({
+	  // Navigation
+	  menu: '#menu',
+	  lockAnchors: false,
+	  anchors: ['first', 'second', 'third'],
+	  navigation: true,
+	  navigationPosition: 'right',
+	  navigationTooltips: false,
+	  showActiveTooltip: false,
+	  slidesNavigation: true,
+	  slidesNavPosition: 'bottom',
+
+	  // Scrolling
+	  css3: true,
+	  scrollingSpeed: 700,
+	  autoScrolling: true,
+	  fitToSection: true,
+	  fitToSectionDelay: 1000,
+	  scrollBar: false,
+	  easing: 'easeInOutCubic',
+	  easingcss3: 'ease',
+	  loopBottom: false,
+	  loopTop: false,
+	  loopHorizontal: true,
+	  continuousVertical: false,
+	  normalScrollElements: '#element1, .element2',
+	  scrollOverflow: false,
+	  touchSensitivity: 15,
+	  normalScrollElementTouchThreshold: 5,
+
+	  // Accessibility
+	  keyboardScrolling: true,
+	  animateAnchor: true,
+	  recordHistory: true,
+
+	  // Design
+	  controlArrows: true,
+	  verticalCentered: true,
+	  resize : false,
+	  sectionsColor : ['#000000', '#FFFFFF', '#FFFFFF'],
+	  paddingTop: '0',
+	  paddingBottom: '0',
+	  fixedElements: '#header, .footer',
+	  responsiveWidth: 0,
+	  responsiveHeight: 0,
+
+	  // Custom Selectors
+	  sectionSelector: '.section',
+	  slideSelector: '.slide',
+
+	  // Events
+	  onLeave: function(index, nextIndex, direction){},
+	  afterLoad: function(anchorLink, index){},
+	  afterRender: function(){},
+	  afterResize: function(){},
+	  afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+	  onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+	});
+});
