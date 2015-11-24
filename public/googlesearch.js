a230f324f02e21bda797597a13f6ec0744a3e81c
thomasvaeth <thomas.vaeth@gmail.com>
2015-11-24 11:10:57 -0800

    Fixed add address.

--- public/googlesearch.js
+++ public/googlesearch.js
@@ -1,12 +1,4 @@
 $(function(){
-var options = {
-  types: ['address']
-};
-
-var from = document.getElementById('from');
-autocomplete = new google.maps.places.Autocomplete(from, options);
-var to = document.getElementById('to')
-autocomplete2 = new google.maps.places.Autocomplete(to, options);
 
 $("#savedTo").on("change",function(){$("#to").val($("#savedTo").val())})
 $("#savedFrom").on("change",function(){$("#from").val($("#savedFrom").val())})
